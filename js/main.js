244fe431e579428c98cbcd80db61627d517c42d3
LauraEsteva <lauraestevaa@gmail.com>
2021-12-01 15:14:04 +0100

    arreglos menu

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-//create a function that will validate a input number is a DNI
+const hamburguerButton = document.getElementById('hamburguer__button');
+const desplegable = document.getElementById('desplegable');
+const overlay = document.getElementById('overlay');
+
+hamburguerButton.addEventListener('click', () =>  {
+
+    hamburguerButton.classList.toggle('--active');
+    desplegable.classList.toggle('--active');
+    overlay.classList.toggle('--active');
+});
+
+window.addEventListener("scroll", () => {
+
+    let winScroll = document.documentElement.scrollTop;
+    let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    let scrolled = (winScroll / height) * 100;
+  
+    document.getElementById("progressBar").style.width = scrolled + "%";
+});
